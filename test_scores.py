43da8926dc6af3344ac5978956cb8821aca0c969
Melvyn Salvatierra <melvyn.salvatierra@gmail.com>
2025-04-11 15:32:32 -0500

    Create test_scores.py

---
+++ test_scores.py
@@ -0,0 +1,28 @@
+try:
+
+    score = input("Enter Score: ")
+    s = float(score)
+
+    if s < 0 or s > 1:
+        raise ValueError("Invalid Input")
+    elif s >= 0.9:
+        print("A")
+    elif s >= 0.8:
+        print("B")
+    elif s >= 0.7:
+        print("C")
+    elif s >= 0.6:
+        print("D")
+    else:
+        print("F")
+        
+except ValueError as e:
+    print(e)
+
+
+    
+    
+
+
+        
+        
